84e893a9762edaea22a98fb4add02f0ef3f08368
Othon Gomes <othonmarcello@hotmail.com>
2024-03-12 09:33:21 -0300

    Implementação do PUT e DELETE

--- src/main/java/curso/spring/rest/api/controller/IndexController.java
+++ src/main/java/curso/spring/rest/api/controller/IndexController.java
@@ -1,14 +1,17 @@
 package curso.spring.rest.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,7 @@ public class IndexController {
 	private UsuarioRepository usuarioRepository;
 
 	/* Serviços RESTful */
+	
 	/*Visualizar por ID*/
 	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
@@ -30,6 +34,15 @@ public class IndexController {
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
 		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+	}
+	
+	/*DELETE - DELETAR usuário*/
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable(value = "id") Long id) {
+
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
 	}
 	
 	/*Visualizar todos os usuários*/
@@ -40,6 +53,28 @@ public class IndexController {
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
+	
+	
+	/*Criar usuário*/
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	/*PUT - ATUALIZAR usuário*/
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+		
+		/*O .save salva e atualiza (sabe é que pra atualizar pois esta recebendo o id)*/
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	
 
 
 }
